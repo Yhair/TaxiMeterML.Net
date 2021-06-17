db4b27ff75fa00a563c313f8dc6e321e2a0ced70
Yhair Tamayo <yhair.tamayo@outlook.com>
2021-06-17 02:34:14 -0500

    Data Preparation

--- TaxiMeterPrediction/Program.cs
+++ TaxiMeterPrediction/Program.cs
@@ -13,7 +13,7 @@ using Microsoft.ML.Data;
 using PLplot;
 
 using Regression_TaxiFarePrediction.DataStructures;
-
+using TaxiMeterPrediction.Helpers;
 using static Microsoft.ML.Transforms.NormalizingEstimator;
 
 namespace TaxiMeterPrediction
@@ -22,7 +22,7 @@ namespace TaxiMeterPrediction
     {
         private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
 
-        private static string BaseDatasetsRelativePath = @"../../../../Data";
+        private static string BaseDatasetsRelativePath = @"Data";
         private static string TrainDataRelativePath = $"{BaseDatasetsRelativePath}/taxi-fare-train.csv";
         private static string TestDataRelativePath = $"{BaseDatasetsRelativePath}/taxi-fare-test.csv";
 
@@ -33,10 +33,60 @@ namespace TaxiMeterPrediction
         private static string ModelRelativePath = $"{BaseModelsRelativePath}/TaxiFareModel.zip";
 
         private static string ModelPath = GetAbsolutePath(ModelRelativePath);
-        
+
         static void Main(string[] args)
         {
+            MLContext mlContext = new MLContext(seed: 0);
 
+            // Crear, Entrenar, Evaluar y Guardar el modelo
+            BuildTrainEvaluateAndSaveModel(mlContext);
+        }
+
+        private static ITransformer BuildTrainEvaluateAndSaveModel(MLContext mlContext)
+        {
+            // PASO 1: Carga de datos
+            IDataView baseTrainingDataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(TrainDataPath, hasHeader: true, separatorChar: ',');
+            IDataView testDataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(TestDataPath, hasHeader: true, separatorChar: ',');
+
+            // PASO 2: Preparar los datos
+            // Remover "outliers", usando la columna FareAmounts, filtrar los valores mayores a 150 y menores que $1. 
+            // Seguramente es data erronea
+            var cnt = baseTrainingDataView.GetColumn<float>(nameof(TaxiTrip.FareAmount)).Count();
+            Console.WriteLine($"Nro registros antes de eliminar outliers: {cnt}");
+
+            IDataView trainingDataView = mlContext.Data.FilterRowsByColumn(baseTrainingDataView, nameof(TaxiTrip.FareAmount), lowerBound: 1, upperBound: 150);
+
+            var cnt2 = trainingDataView.GetColumn<float>(nameof(TaxiTrip.FareAmount)).Count();
+            Console.WriteLine($"Nro registros despues de eliminar outliers: {cnt2}");
+
+            // Transformar columnas categóricas y normalizar los datos
+
+            var dataProcessPipeline = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: nameof(TaxiTrip.FareAmount))
+
+                // "One hot encoding" para transformar columnas categóricas
+                //Rate Code(1,2,3)     VendorId (CMT,VTS)   
+                // 1 0 0                   1 0
+                // 1 0 0                   0 1
+                // 0 1 0                   1 0
+                // 0 1 0                   0 1
+                // 0 0 1                   1 0
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded", inputColumnName: nameof(TaxiTrip.VendorId)))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded", inputColumnName: nameof(TaxiTrip.RateCode)))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "PaymentTypeEncoded", inputColumnName: nameof(TaxiTrip.PaymentType)))
+
+                // Normalizar el valor de las columnas   (X - promedio)/dev.std.
+                .Append(mlContext.Transforms.NormalizeMeanVariance(outputColumnName: nameof(TaxiTrip.PassengerCount)))
+                .Append(mlContext.Transforms.NormalizeMeanVariance(outputColumnName: nameof(TaxiTrip.TripTime)))
+                .Append(mlContext.Transforms.NormalizeMeanVariance(outputColumnName: nameof(TaxiTrip.TripDistance)))
+
+                // Finalmente devolvemos todas las columnas procesadas
+                .Append(mlContext.Transforms.Concatenate("Features", "VendorIdEncoded", "RateCodeEncoded", "PaymentTypeEncoded", nameof(TaxiTrip.PassengerCount), nameof(TaxiTrip.TripTime), nameof(TaxiTrip.TripDistance)));
+
+            // (OPTIONAL) Peek data (such as 5 records) in training DataView after applying the ProcessPipeline's transformations into "Features" 
+            ConsoleHelper.PeekDataViewInConsole(mlContext, trainingDataView, dataProcessPipeline, 5);
+            ConsoleHelper.PeekVectorColumnDataInConsole(mlContext, "Features", trainingDataView, dataProcessPipeline, 5);
+
+            return null;
         }
 
         public static string GetAbsolutePath(string relativePath)
@@ -48,5 +98,6 @@ namespace TaxiMeterPrediction
 
             return fullPath;
         }
+
     }
 }
