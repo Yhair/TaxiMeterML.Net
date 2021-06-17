d59a52122de2d77a057a6853b913a1258d8dee33
Yhair Tamayo <yhair.tamayo@outlook.com>
2021-06-17 00:56:45 -0500

    Add project files.

---
+++ TaxiMeterPrediction/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+using Common;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+using PLplot;
+
+using Regression_TaxiFarePrediction.DataStructures;
+
+using static Microsoft.ML.Transforms.NormalizingEstimator;
+
+namespace TaxiMeterPrediction
+{
+    class Program
+    {
+        private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+
+        private static string BaseDatasetsRelativePath = @"../../../../Data";
+        private static string TrainDataRelativePath = $"{BaseDatasetsRelativePath}/taxi-fare-train.csv";
+        private static string TestDataRelativePath = $"{BaseDatasetsRelativePath}/taxi-fare-test.csv";
+
+        private static string TrainDataPath = GetAbsolutePath(TrainDataRelativePath);
+        private static string TestDataPath = GetAbsolutePath(TestDataRelativePath);
+
+        private static string BaseModelsRelativePath = @"../../../../MLModels";
+        private static string ModelRelativePath = $"{BaseModelsRelativePath}/TaxiFareModel.zip";
+
+        private static string ModelPath = GetAbsolutePath(ModelRelativePath);
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+    }
+}
